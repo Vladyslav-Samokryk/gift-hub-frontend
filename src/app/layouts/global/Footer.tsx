7d8b761e204deb1e45559b4b69ea9faa4b368285
SLY <SLY@d.c>
2023-08-14 09:14:47 +0300

    revert changes for Footer and Header

--- src/app/layouts/global/Footer.tsx
+++ src/app/layouts/global/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer (): JSX.Element {
   const t = useTypedTranslation();
 
   return (
-    <footer className="p-3 bg-background-footer">
+    <footer className="p-3 bg-[#D9D9D9]">
       <h2>{t("footer")}</h2>
     </footer>
   );
