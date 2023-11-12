553259dda7eb2ac0e1b9dc21cf32e9b787629baf
zim89 <121241841+zim89@users.noreply.github.com>
2023-11-12 14:54:58 +0200

    add Product to Cart
    add Product to Cart onclick
    show adding items in cart on Header

--- src/app/store/cart/cartSlice.ts
+++ src/app/store/cart/cartSlice.ts
@@ -1,12 +1,20 @@
+import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 import type { ProductCardType } from "@src/shared";
+import { calcTotalPrice } from "@src/shared/helpers/calcTotalPrice";
+
+export interface CartItem extends ProductCardType {
+  count: number;
+}
 
 export interface CartState {
-  items: ProductCardType[];
+  items: CartItem[];
+  totalPrice: number;
 }
 
 const initialState: CartState = {
   items: [],
+  totalPrice: 0,
 };
 
 const cartSlice = createSlice({
@@ -14,17 +22,43 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.items.push(action.payload);
+      const item = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (item) {
+        item.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = calcTotalPrice(state.items);
+    },
+    decrementItem(state, action: PayloadAction<string>) {
+      const item = state.items.find((obj) => obj.id === action.payload);
+
+      if (item) {
+        item.count--;
+      }
+
+      state.totalPrice = calcTotalPrice(state.items);
     },
     removeFromCart: (state, action) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = calcTotalPrice(state.items);
+    },
+
+    clearCart(state) {
+      state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, decrementItem, removeFromCart, clearCart } =
+  cartSlice.actions;
 
-export const selectCart = (state: RootState): ProductCardType[] =>
-  state.cart.items;
+export const selectCart = (state: RootState): CartItem[] => state.cart.items;
 
 export const cartReducer = cartSlice.reducer;
