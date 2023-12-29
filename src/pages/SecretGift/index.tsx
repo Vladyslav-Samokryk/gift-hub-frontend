3756df1bff0efd756a12eab41936f2ec4c3c9a32
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2023-12-29 15:10:49 +0200

    fix secret present

--- src/pages/SecretGift/index.tsx
+++ src/pages/SecretGift/index.tsx
@@ -1,31 +1,25 @@
 import { useState } from "react";
 import SecretGiftContainer from "components/SecretGiftContainer";
 import SecretGiftDescription from "components/SecretGiftDescription";
+import classNames from "classnames";
 
 export default function SecretGift(): JSX.Element {
   const [isVisibleSelect, setIsVisibleSelect] = useState(false);
 
   return (
-    <>
-      <div
-        className="relative left-0 top-0 w-full"
-        style={
-          isVisibleSelect
-            ? {
-                backgroundImage:
-                  "url('https://main--lighthearted-cocada-0d2e37.netlify.app/img/animation.svg')]",
-              }
-            : {}
-        }
-      >
-        <div className="relative flex w-full flex-col items-center justify-center px-5 pb-5 text-black backdrop-blur-md">
-          {!isVisibleSelect ? (
-            <SecretGiftDescription setIsVisibleSelect={setIsVisibleSelect} />
-          ) : (
-            <SecretGiftContainer />
-          )}
-        </div>
+    <div
+      className={classNames("relative left-0 top-0 w-full", {
+        "bg-[url('https://main--lighthearted-cocada-0d2e37.netlify.app/img/animation.svg')]]":
+          isVisibleSelect,
+      })}
+    >
+      <div className="relative flex w-full flex-col items-center justify-center px-5 pb-5 text-black backdrop-blur-md">
+        {!isVisibleSelect ? (
+          <SecretGiftDescription setIsVisibleSelect={setIsVisibleSelect} />
+        ) : (
+          <SecretGiftContainer isVisibleSelect={isVisibleSelect} />
+        )}
       </div>
-    </>
+    </div>
   );
 }
