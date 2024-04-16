561cd73c75471db31f06b5554b3b155928506206
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2024-04-16 14:16:35 +0300

    remove logs

--- src/app/api/googleLogin.ts
+++ src/app/api/googleLogin.ts
@@ -5,9 +5,7 @@ import { API_URL } from "./config";
 export const getTokens = async (code: string) => {
   const tokens = await axios.post(`${API_URL}accounts/google_auth_code/`, {
     code,
-    // redirect_uri: "postmessage",
     state: "123",
   });
-  console.log(tokens);
   return tokens;
 };
