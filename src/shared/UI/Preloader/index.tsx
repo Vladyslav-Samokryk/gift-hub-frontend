2defe1a6334c48fe62d22930820f905102061a6e
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2024-05-05 18:50:15 +0300

    fix preload, select in pagination catalog

--- src/shared/UI/Preloader/index.tsx
+++ src/shared/UI/Preloader/index.tsx
@@ -3,7 +3,7 @@ import "./index.scss";
 export default function Preloader(): JSX.Element {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <div className="dots" />;
+      <div className="dots" />
     </div>
   );
 }
