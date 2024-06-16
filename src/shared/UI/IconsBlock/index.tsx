fe5b672b034049f33ace652a66661953e114f087
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2024-06-16 11:59:07 +0300

    fix warnings in icon block

--- src/shared/UI/IconsBlock/index.tsx
+++ src/shared/UI/IconsBlock/index.tsx
@@ -7,6 +7,8 @@ import { PAGE_OPTIONS } from "shared/constants/pageOptions";
 
 import { useScreenWidth } from "shared/hooks/useScreenWidth";
 
+const iconBlockClass="hover:text-blue-800 cursor-pointer transition-colors	duration-500 ease-in-out"
+
 export default function IconsBlock(): JSX.Element {
   const screenWidth = useScreenWidth();
   const iconSize = screenWidth <= SCREEN.LG ? PAGE_OPTIONS[3] : PAGE_OPTIONS[2];
@@ -14,19 +16,19 @@ export default function IconsBlock(): JSX.Element {
   return (
     <div className="flex items-center mx-auto gap-3 w-fit">
       <CiLinkedin
-        className="hover:text-blue-800 cursor-pointer transition-colors	duration-500 ease-in-out"
+        className={iconBlockClass}
         size={iconSize}
       />
       <MdAlternateEmail
-        className="hover:text-blue-800 cursor-pointer transition-colors	duration-500 ease-in-out"
+        className={iconBlockClass}
         size={iconSize}
       />
       <FaInstagram
-        className="hover:text-blue-800 cursor-pointer transition-colors	duration-500 ease-in-out"
+        className={iconBlockClass}
         size={iconSize}
       />
       <FaGithub
-        className="hover:text-blue-800 cursor-pointer transition-colors	duration-500 ease-in-out"
+        className={iconBlockClass}
         size={iconSize}
       />
     </div>
